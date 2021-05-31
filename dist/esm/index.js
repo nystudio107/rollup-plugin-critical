bc297fea82e99279b195295142bab01fd3d4e13d
Andrew Welch <andrew@nystudio107.com>
2021-05-30 22:48:38 -0400

    Include dist/
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

---
+++ dist/esm/index.js
@@ -0,0 +1,61 @@
+import path from 'path';
+const critical = require('critical');
+const criticalSuffix = '_critical.min.css';
+const defaultCriticalConfig = {
+    inline: false,
+    minify: true,
+    extract: false,
+    width: 1200,
+    height: 1200,
+    concurrency: 4,
+    penthouse: {
+        blockJSRequests: false
+    }
+};
+function PluginCritical(pluginConfig, callback) {
+    return {
+        name: 'critical',
+        async writeBundle(outputOptions, bundle) {
+            const css = [];
+            // Find all of the generated CSS assets
+            if (bundle) {
+                for (const chunk of Object.values(bundle)) {
+                    if (chunk.type === 'asset' && chunk.fileName.endsWith('.css')) {
+                        if (outputOptions.dir !== undefined) {
+                            const cssFile = path.join(outputOptions.dir, chunk.fileName);
+                            css.push(cssFile);
+                        }
+                    }
+                }
+                // If we have no CSS, skip bundle
+                if (!css.length) {
+                    return;
+                }
+            }
+            // Iterate through the pages
+            for (const page of pluginConfig.criticalPages) {
+                const criticalBase = pluginConfig.criticalBase;
+                const criticalSrc = pluginConfig.criticalUrl + page.uri;
+                const criticalDest = page.template + criticalSuffix;
+                // Merge in our options
+                const options = Object.assign({ css }, defaultCriticalConfig, {
+                    base: criticalBase,
+                    src: criticalSrc,
+                    target: criticalDest,
+                }, pluginConfig.criticalConfig);
+                // Generate the Critical CSS
+                console.log(`Generating critical CSS from ${criticalSrc} to ${criticalDest}`);
+                await critical.generate(options, (err) => {
+                    if (err) {
+                        console.error(err);
+                    }
+                    if (callback) {
+                        callback(err);
+                    }
+                });
+            }
+        }
+    };
+}
+;
+export default PluginCritical;
