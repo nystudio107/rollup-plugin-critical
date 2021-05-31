c4a9e3ccf7144744ebcdb128fbfb115a17a6e0bc
Andrew Welch <andrew@keluli.local>
2021-05-31 12:31:34 -0400

    Dist build

--- dist/esm/index.js
+++ dist/esm/index.js
@@ -1,36 +1,42 @@
 import path from 'path';
 const critical = require('critical');
 const criticalSuffix = '_critical.min.css';
+/**
+ * Default `criticalConfig` passed in to `critical`
+ */
 const defaultCriticalConfig = {
     inline: false,
     minify: true,
     extract: false,
     width: 1200,
     height: 1200,
-    concurrency: 4,
     penthouse: {
         blockJSRequests: false
     }
 };
+/**
+ * [Vite.js](https://vitejs.dev/) & [Rollup](https://rollupjs.org/) plugin for generating critical CSS
+ * that uses the [critical](https://github.com/addyosmani/critical) generator under the hood.
+ *
+ * @param {CriticalPluginConfig} pluginConfig - the plugin configuration object
+ * @param {Function} callback - callback upon completion of the critical CSS generation
+ * @constructor
+ */
 function PluginCritical(pluginConfig, callback) {
     return {
         name: 'critical',
         async writeBundle(outputOptions, bundle) {
             const css = [];
             // Find all of the generated CSS assets
-            if (bundle) {
-                for (const chunk of Object.values(bundle)) {
-                    if (chunk.type === 'asset' && chunk.fileName.endsWith('.css')) {
-                        if (outputOptions.dir !== undefined) {
-                            const cssFile = path.join(outputOptions.dir, chunk.fileName);
-                            css.push(cssFile);
-                        }
-                    }
+            for (const chunk of Object.values(bundle)) {
+                if (chunk.type === 'asset' && chunk.fileName.endsWith('.css')) {
+                    const cssFile = path.join(outputOptions.dir || '', chunk.fileName);
+                    css.push(cssFile);
                 }
-                // If we have no CSS, skip bundle
-                if (!css.length) {
-                    return;
-                }
+            }
+            // If we have no CSS, skip bundle
+            if (!css.length) {
+                return;
             }
             // Iterate through the pages
             for (const page of pluginConfig.criticalPages) {
@@ -57,5 +63,4 @@ function PluginCritical(pluginConfig, callback) {
         }
     };
 }
-;
 export default PluginCritical;
