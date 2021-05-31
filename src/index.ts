690120dd377ce152e935a63436023b6f327938ff
Andrew Welch <andrew@keluli.local>
2021-05-31 12:11:31 -0400

    Remove vestigial `concurrency` setting

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,15 @@ const critical = require('critical');
 
 const criticalSuffix = '_critical.min.css';
 
+/**
+ * Default `criticalConfig` passed in to `critical`
+ */
 const defaultCriticalConfig: Partial<CriticalConfig> = {
   inline: false,
   minify: true,
   extract: false,
   width: 1200,
   height: 1200,
-  concurrency: 4,
   penthouse: {
     blockJSRequests: false
   }
@@ -24,9 +26,19 @@ interface CriticalPages {
 }
 
 interface CriticalPluginConfig {
-  /** The base URL to use in combination with the `criticalPages` `uri`s to determine the URLs to scrape for Critical CSS */
+  /**
+   * The base URL to use in combination with the `criticalPages` `uri`s to determine the URLs to scrape for Critical CSS.
+   * This can also be a file system path. This is combined with `criticalPages.uri`
+   * to determine pages to scrap for critical CSS.
+   * Determines the `criticalConfig.src` property
+   */
   criticalUrl: string;
-  /** The base file system path to where the generated Critical CSS file should be stored */
+  /**
+   * The base file system path to where the generated Critical CSS file should be saved.
+   * This is combined with `criticalPages.template` with `_critical.min.css` appended
+   * to it to determine the saved critical CSS file name.
+   * Determines the `criticalConfig.target` property
+   */
   criticalBase?: string;
   /**
    * An array objects that contain the page `uri`s that are combined with the `criticalUrl` to
@@ -34,7 +46,11 @@ interface CriticalPluginConfig {
    * `template` path, and saved to the `criticalBase` directory
    */
   criticalPages: Partial<CriticalPages>[];
-  /** This is the full [config for critical](https://github.com/addyosmani/critical#options) that is passed through to the `critical` package.*/
+  /**
+   * This is the full [config for critical](https://github.com/addyosmani/critical#options) that is passed
+   * through to the `critical` package.
+   * You may optionally override any properties you like here
+   */
   criticalConfig?: Partial<CriticalConfig>;
 }
 
