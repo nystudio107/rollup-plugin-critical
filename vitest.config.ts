e3b29d68c790968e1519ecf77fa47f11dba6978a
Andrew Welch <andrew@nystudio107.com>
2022-12-13 13:50:25 -0500

    refactor: Add coverage to default `test` script for CI

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,8 +3,5 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     testTimeout: 20000,
-    coverage: {
-      reporter: ['text', 'json', 'html', 'clover'],
-    },
   },
 });
